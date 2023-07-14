3ed9558fe101d02fe03ffaaa8a2cd51732761301
Samson G. Njogu <samsongikonyo142@gmail.com>
2023-07-13 20:07:23 -0700

    4-puts.c

--- 0x00-hello_world/sudo tee 4-puts.c
+++ 0x00-hello_world/sudo tee 4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
-*main-using puts function to print a line
-n
-*Return:0
-*/
+ * main - using puts function to print a line
+ * Return: 0 
+ */
 int main(void)
+{       
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
+
