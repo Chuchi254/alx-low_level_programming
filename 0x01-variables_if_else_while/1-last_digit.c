b464e522be6286539b9c80a7778cf74e05e10169
Samson G. Njogu <samsongikonyo142@gmail.com>
2023-07-17 21:39:44 -0700

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,10 +8,12 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is ", n);
 	int lastDigit = abs(n) % 10;
+
 	if (lastDigit > 5)
 	{
 		printf("%d is greater than 5\n", lastDigit);
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the letters of the the alphabet
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; leter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
