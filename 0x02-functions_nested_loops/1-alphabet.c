3a8bd512afc5d3b8167c3094673c8e5bf0fea362
Samson G. Njogu <samsongikonyo142@gmail.com>
2023-07-18 20:44:49 -0700

    updates

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the letters of hte alphabet in lowercase
+ * Return: 0 on success
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
