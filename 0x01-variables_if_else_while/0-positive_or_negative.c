f2fb712550ca11c5395e94c3fbd772f35def0c17
Samson G. Njogu <samsongikonyo142@gmail.com>
2023-07-23 05:55:26 -0700

    0-positive_or_negative.c update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,26 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main - program that assigns a random number to the variable n
- * Return: 0 on success
- */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-printf("%d is ", n);
-if (n > 0)
-{
-	printf("positive\n");
-}
-else if (n == 0)
-{
-	printf("zero\n");
-}
-else
-{
-	printf("negative\n");
-}
-return (0);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	else
+	{
+		printf("is negative\n");
+	}
+	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,26 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main - prints the last digit of the random variable stored in the value n
- * Return: 0 on success
- */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+        int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is ", n);
-	int lastDigit = abs(n) % 10;
-
-	if (lastDigit > 5)
-	{
-		printf("%d is greater than 5\n", lastDigit);
-	}
-	else if (lastDigit == 0)
-	{
-		printf("0\n");
-	}
-	else
-	{
-		printf("%d is less than 5\n", lastDigit);
-	}
-	return (0);
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        l = n % 10;
+        if (l > 5)
+        {
+                printf("Last digit of %d is %d and is greater than 5\n", n, l);
+        }
+        elseif (l == 0)
+        {
+                printf("Last digit of %d is %d and is 0\n", n, l);
+        }
+        else
+        {
+                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+        }
+        return (0);
 }
