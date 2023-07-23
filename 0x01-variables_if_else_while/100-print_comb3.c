8036252b5e275445c2f843021893937d657a0dc0
Samson G. Njogu <samsongikonyo142@gmail.com>
2023-07-23 13:58:59 -0700

    100-print_comb3 update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints combination of 2 digits
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int firstDigit, lastDigit;
+
+	for (firstDigit = 48; firstDigit <= 56; firstDigit++)
+	{
+		for (lastDigit = 49; lastDigit <= 57; lastDigit++)
+		{
+			if (firstDigit == lastDigit)
+			{
+				continue;
+			}
+			putchar(firstDigit);
+			putchar(lastDigit);
+			if (firstDigit == 8 && lastDigit == 9)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+	}
+	return (0);
+}
