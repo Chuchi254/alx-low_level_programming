26fe17b16887b83dbc532598919e0d10c3f06b77
Samson G. Njogu <samsongikonyo142@gmail.com>
2023-07-24 10:00:05 -0700

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: stores the string
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+	char *end = s;
+	char temp;
+
+	while (*end != '\0')
+	{
+		length++;
+		end++;
+	}
+	end--;
+
+	while (s < end)
+	{
+		temp = *s;
+		*s = *end;
+		*end = temp;
+
+		s++;
+		end--;
+	}
+}
