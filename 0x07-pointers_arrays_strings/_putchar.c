925f810499f90f9d4406186986358edf67c59f57
Samson G. Njogu <samsongikonyo142@gmail.com>
2023-07-31 19:40:56 -0700

    main files

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes c to stdout
+ * @c: character to print
+ *
+ * Return: 1 on success
+ *
+ * On error, -1 is returned, and errno set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
