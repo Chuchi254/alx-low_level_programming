eb6b47cb44a0310dc7d2536d55d589542e4a484f
Samson G. Njogu <samsongikonyo142@gmail.com>
2023-08-30 03:34:33 -0700

    mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - returns the product of 2 integers
+ * @a: input
+ * @b: input
+ *
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
