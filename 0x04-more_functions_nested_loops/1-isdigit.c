ac4fb93d79f429b43fa2307549e2cf878e17fc33
Samson G. Njogu <samsongikonyo142@gmail.com>
2023-08-30 03:23:47 -0700

    updates

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks whether input is a digit
+ * @c: input
+ * Return: 0 on success
+ */
+
+int _isdigit(int c)
+{
+	while (c >= 0 && c <= 9)
+		return (1);
+	return (0);
+}
