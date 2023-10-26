3ce13d223167fef01ce07dc633e058b867f6f7d6
Chuchi254 <samsongikonyo142@gmail.com>
2023-10-26 02:47:24 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line using underscores
+ *
+ * @n: number of hyphens to be used
+ *
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('$');
+		_putchar('\n');
+	}
+	else
+	{
+		int count;
+
+		for (count = 0; count < n; count++)
+		{
+			_putchar('_');			
+		}
+		_putchar('$');
+		_putchar('\n');
+	}
+}
