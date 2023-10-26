005a52181b047438b65b13702b56c50560e78d8a
Chuchi254 <samsongikonyo142@gmail.com>
2023-10-26 02:13:51 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the number 0 to 14 followed by a new line
+ */
+
+void more_numbers(void)
+{
+	int count, i;
+	char num = '0';
+
+	for (i = 0; i < 5; i++)
+	{
+		for (count = 0; count <= 14; count++)
+		{
+			if (count >= 10)
+			{
+				_putchar('1');
+				num = (count % 10) + '0';
+			}
+			else
+			{
+				num = count + '0';
+			}
+			_putchar(num);
+		}
+		_putchar('\n');
+	}
+}
