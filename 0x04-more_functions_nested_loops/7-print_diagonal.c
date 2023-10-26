7a7520673b296d8f602a15692f4bbb27592b2ad5
Chuchi254 <samsongikonyo142@gmail.com>
2023-10-26 03:52:53 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints a line using underscores
- *
- * @n: number of hyphens to be used
- *
- */
+*print_diagonal- prints a diagonal
+*
+*@n:number of back slashes
+*
+*/
 
 void print_diagonal(int n)
 {
-        int count;
+	int count;
 
-        if (n <= 0)
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                for (count = 0; count < n; count++)
-                {
-                        _putchar('\\');
-                }
-                _putchar('\n');
-        }
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (count = 0; count < n; count++)
+		{
+		_putchar('\\');
+		}
+	_putchar('\n');
+	}
 }
