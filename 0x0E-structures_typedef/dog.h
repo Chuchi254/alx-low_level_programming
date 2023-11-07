716ec2851a2168b4df13d35dbd1052aadf128312
Chuchi254 <samsongikonyo142@gmai.com>
2023-11-07 02:44:23 -0800

    update on dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dog's basic information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner's name
+ */
+
 struct dog
 {
 	char *name;
