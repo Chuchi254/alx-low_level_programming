c8ba199be0bafa901151c5e118c86ea351d14118
Chuchi254 <samsongikonyo142@gmail.com>
2024-01-23 05:04:04 -0800

    contain structure with dog attributes

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure that takes in dog information
+ *
+ * @name: Name of the dog
+ * @age: age of the dock
+ * @owner: Owner's name
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
