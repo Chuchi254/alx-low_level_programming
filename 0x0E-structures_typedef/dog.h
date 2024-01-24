82248f60921ca9399468c30c0b2c5a2df05aadde
Chuchi254 <samsongikonyo142@gmail.com>
2024-01-23 18:59:55 -0800

    all files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdlib.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 /**
  * struct dog - structure that takes in dog information
  *
