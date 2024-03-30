3b263b994bfc83f4102cce7d8f56fb11a3b05720
Gikonyo Njogu <samsongikonyo142@gmail.com>
2024-03-30 02:32:54 -0700

    finds the absolute of an integer

---
+++ 0x18-dynamic_libraries/_abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute of an integer input
+ * @c: input
+ * Return: n
+ */
+
+int _abs(int n)
+{
+	if(n >= 0)
+		return(n);
+	else
+		return(-n);
+}
