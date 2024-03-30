50c2481b1ed54baa40cd4e11010d6b7e0ab57514
Gikonyo Njogu <samsongikonyo142@gmail.com>
2024-03-30 02:38:50 -0700

    concatenates 2 strings

---
+++ 0x18-dynamic_libraries/_strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates 2 strings
+ * @dest: string to be added to
+ * @src: string to be added
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *scr)
+{       
+        int count = 0;
+        int len;
+
+        len = _strlen(dest);
+        if(src == NULL)
+                return(NULL);
+        while(src[count] != '\0')
+        {       
+                dest[len + count] = src[count];
+                count++;
+        }       
+        dest[len + count] = '\0';
+	return(dest);
+}
