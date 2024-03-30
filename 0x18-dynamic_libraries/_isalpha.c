26e2c2954c6a7daac7516ae979193ce02f6d3aee
Gikonyo Njogu <samsongikonyo142@gmail.com>
2024-03-30 04:54:37 -0700

    Implement _abs function to return the absolute value of an integer

--- 0x18-dynamic_libraries/_isalpha.c
+++ 0x18-dynamic_libraries/_isalpha.c
@@ -3,14 +3,14 @@
 /**
  * _isalpha - checks whether input is an alphabet
  * @c: input to be checked
- * Return; 1 if c is alphabet, else 0
+ * Return: 1 if c is alphabet, else 0
  */
 
 int _isalpha(int c)
 {
-	if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-		return(1);
+		return (1);
 	}
-	return(0);
+	return (0);
 }
