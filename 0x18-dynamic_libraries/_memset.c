6562a23367987f09dbe0145579afd44de2f85499
Gikonyo Njogu <samsongikonyo142@gmail.com>
2024-03-30 05:06:19 -0700

    Implement _memset function to fill memory with a constant byte

--- 0x18-dynamic_libraries/_memset.c
+++ 0x18-dynamic_libraries/_memset.c
@@ -4,6 +4,7 @@
  * _memset - fills the first n bytes with b in the memory area pointed by s
  * @b: constant character
  * @s: string to be filled with b
+ * @n: size of byte filled
  * Return: s
  */
 
@@ -11,10 +12,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int count = 0;
 
-	while(count < n)
+	while (count < n)
 	{
 		s[count] = b;
 		count++;
 	}
-	return(s);
+	return (s);
 }
