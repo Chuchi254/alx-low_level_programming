50fdde56bd82ba5a966bf783d84189de3d66b465
Gikonyo Njogu <samsongikonyo142@gmail.com>
2024-03-30 02:36:09 -0700

    fills the a certain number of bytes in the pointed memory area

---
+++ 0x18-dynamic_libraries/_memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes with b in the memory area pointed by s
+ * @b: constant character
+ * @s: string to be filled with b
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int count = 0;
+
+	while(count < n)
+	{
+		s[count] = b;
+		count++;
+	}
+	return(s);
+}
