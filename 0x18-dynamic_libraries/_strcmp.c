b5019723044e67590835efcc03bf6d3e3d507379
Gikonyo Njogu <samsongikonyo142@gmail.com>
2024-03-30 05:08:35 -0700

    Implement _strcmp function to compare two strings

--- 0x18-dynamic_libraries/_strcmp.c
+++ 0x18-dynamic_libraries/_strcmp.c
@@ -5,17 +5,17 @@
  * @s1: string 1
  * @s2: string 2
  * Return: 0 if s2 and s1 are equal, -1 otherwise
- */ 
+ */
 
 int _strcmp(char *s1, char *s2)
-{       
-        int count = 0;
-        
-        while(s1[count] != '\0' && s2[count] != '\0')
-        {       
-                if(s1[count] != s2[count])
-                        return(s1[count] - s2[count]);
-                count++;
-        }
-	return(0);
+{
+	int count = 0;
+
+	while (s1[count] != '\0' && s2[count] != '\0')
+	{
+	if (s1[count] != s2[count])
+		return (s1[count] - s2[count]);
+	count++;
+	}
+	return (0);
 }
