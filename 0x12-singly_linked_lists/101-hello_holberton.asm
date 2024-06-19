620d00830e035b2359f60fe5ed6b2d3318a10c7c
Chuchi254 <samsongikonyo142@gmail.com>
2024-06-19 07:02:37 -0700

    correction on assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,16 @@
-	section .rodata
-	hello_msg db 'Hello, Holberton', 0 ; Define the message to print with a null terminator
+	section .data
+	format db "Hello, Holberton", 0xA, 0 ; The string to print, followed by a newline and a null terminator
 
 	section .text
-	global _start
-
+	global main
 	extern printf
 
-_start:
-	;;  Call printf function
-	mov rdi, hello_msg	; First argument: pointer to the message
-	xor rax, rax	; Clear rax (no floating point arguments)
-	call printf	; Call the printf function
+main:
+	;;  Set up the argument for printf
+	mov rdi, format    	; First argument (format string) in rdi
+	xor eax, eax       	; Clear eax register (printf expects RAX to be 0 in x64 ABI)
+	call printf
 
-	;;  Exit the program
-	mov rax, 60		; System call number for exit (60)
-	xor rdi, rdi	; Exit code 0
-	syscall	; Invoke the system call
+	;;  Return 0
+	mov eax, 0         	; Return 0
+	ret
