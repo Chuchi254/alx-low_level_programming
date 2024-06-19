8cea3b13c1533fd340e414acf69bc7f08d9a2c18
Chuchi254 <samsongikonyo142@gmail.com>
2024-06-19 04:32:32 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,13 +10,14 @@
  * @age: age of the dock
  * @owner: Owner's name
  */
-
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
+};
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
