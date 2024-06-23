32d4edd808a082b2fcb7c517c3b15a1d3ed40b2b
Chuchi254 <samsongikonyo142@gmail.com>
2024-06-23 04:04:48 -0700

    displays the information contained in the ELF header at the start of an ELF file update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -3,7 +3,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <elf.h>
-#include <string.h>
 
 /**
  * print_error - Prints error message to stderr and exits with code status 98
@@ -25,9 +24,7 @@ void check_elf(unsigned char *e_ident)
 			e_ident[EI_MAG1] != ELFMAG1 ||
 			e_ident[EI_MAG2] != ELFMAG2 ||
 			e_ident[EI_MAG3] != ELFMAG3)
-	{
 		print_error("Error: Not an ELF file");
-	}
 }
 
 /**
@@ -38,7 +35,7 @@ void print_magic(unsigned char *e_ident)
 {
 	int i;
 
-	printf(" Magic:		");
+	printf("Magic:		");
 	for (i = 0; i < EI_NIDENT; i++)
 		printf("%2.2x ", e_ident[i]);
 	printf("\n");
@@ -50,7 +47,7 @@ void print_magic(unsigned char *e_ident)
  */
 void print_class(unsigned char *e_ident)
 {
-	printf(" Class:				");
+	printf("Class:				");
 	switch (e_ident[EI_CLASS])
 	{
 		case ELFCLASSNONE:
@@ -73,7 +70,7 @@ void print_class(unsigned char *e_ident)
  */
 void print_data(unsigned char *e_ident)
 {
-	printf(" Data:				");
+	printf("Data:				");
 	switch (e_ident[EI_DATA])
 	{
 		case ELFDATANONE:
@@ -81,6 +78,9 @@ void print_data(unsigned char *e_ident)
 			break;
 		case ELFDATA2LSB:
 			printf("2's complement, little endian\n");
+			break;
+		case ELFDATA2MSB:
+			printf("2's complement, big endian\n");
 			break;
 		default:
 			printf("<unknown: %x>\n", e_ident[EI_DATA]);
@@ -110,7 +110,7 @@ void print_version(unsigned char *e_ident)
  */
 void print_osabi(unsigned char *e_ident)
 {
-	printf(" OS/ABI:			");
+	printf("OS/ABI:			");
 	switch (e_ident[EI_OSABI])
 	{
 		case ELFOSABI_NONE:
@@ -137,12 +137,24 @@ void print_osabi(unsigned char *e_ident)
 		case ELFOSABI_TRU64:
 			printf("ARM\n");
 			break;
+		case ELFOSABI_ARM:
+			printf("ARM\n");
+			break;
 		case ELFOSABI_STANDALONE:
-			printf("Standalone APP\n");
+			printf("Standalone App\n");
 			break;
 		default:
 			printf("<unknown: %x>\n", e_ident[EI_OSABI]);
 	}
+}
+
+/**
+ * print_abiversion - Prints the ABI version of the ELF header
+ * @e_ident: The identification bytes of the ELF header
+ */
+void print_abiversion(unsigned char *e_ident)
+{
+	printf("ABI Version:			%d\n", e_ident[EI_ABIVERSION]);
 }
 
 /**
@@ -154,7 +166,7 @@ void print_type(uint16_t e_type, unsigned char *e_ident)
 {
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type = e_type >> 8;
-	printf(" Type:				");
+	printf("Type:				");
 	switch (e_type)
 	{
 		case ET_NONE:
@@ -222,12 +234,14 @@ int main(int argc, char **argv)
 		print_error("Error: Can't read from file");
 	}
 
+	printf("Elf Header:\n");
 	check_elf(header.e_ident);
 	print_magic(header.e_ident);
 	print_class(header.e_ident);
 	print_data(header.e_ident);
 	print_version(header.e_ident);
 	print_osabi(header.e_ident);
+	print_abiversion(header.e_ident);
 	print_type(header.e_type, header.e_ident);
 	print_entry(header.e_entry, header.e_ident);
 
