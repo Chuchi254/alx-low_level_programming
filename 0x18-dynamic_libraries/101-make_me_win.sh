8b1a0b1cf42ef75d89cc717af749ac1f4a93c2bc
Chuchi254 <samsongikonyo142@gmail.com>
2024-07-05 10:19:59 -0700

    code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export LD_PRELOAD=$PWD/win.so
-sleep 98
+wget -O win.so https://github.com/Chuchi254/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD="$PWD/win.so"
