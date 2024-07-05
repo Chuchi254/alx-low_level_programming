361feb838edd5fecff77a28d90d4567b8094d4cb
Chuchi254 <samsongikonyo142@gmail.com>
2024-07-05 11:00:24 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O win.so https://github.com/Chuchi254/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD="$PWD/win.so"
+wget -P .. https://github.com/Chuchi254/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD="$PWD/../win.so"
