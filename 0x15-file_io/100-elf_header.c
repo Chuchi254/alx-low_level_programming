0dd8574dc129f45f4523c9382933285b8965087d
Chuchi254 <samsongikonyo142@gmail.com>
2024-07-25 03:42:56 -0700

    to be reviewed later

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,16 +6,27 @@
 #include <string.h>
 #include <errno.h>
 
+/**
+ * print_error_and_exit - prints error and exits code
+ * @msg: message to be printed
+ */
+
 void print_error_and_exit(const char *msg)
 {
 	fprintf(stderr, "Error: %s\n", msg);
 	exit(98);
 }
 
+/**
+ * print_elf_header_info - prints elf header information
+ * @header: header to printed
+ */
+
 void print_elf_header_info(Elf64_Ehdr *header)
 {
 	int i;
 
+	printf("Magic:   ");
 	printf("Magic:   ");
 	for (i = 0; i < EI_NIDENT; i++)
 		printf("%02x ", header->e_ident[i]);
@@ -23,6 +34,16 @@ void print_elf_header_info(Elf64_Ehdr *header)
 
 	printf("Class:                             ");
 	switch (header->e_ident[EI_CLASS])
+}
+
+/**
+ * print_class - Prints the class of the ELF header
+ * @e_ident: The identification bytes of the ELF header
+ */
+void print_class(unsigned char *e_ident)
+{
+	printf("Class:                             ");
+	switch (e_ident[EI_CLASS])
 	{
 		case ELFCLASSNONE:
 			printf("NONE\n");
@@ -39,6 +60,16 @@ void print_elf_header_info(Elf64_Ehdr *header)
 
 	printf("Data:                              ");
 	switch (header->e_ident[EI_DATA])
+}
+
+/**
+ * print_data - Prints the data encoding of the ELF header
+ * @e_ident: The identification bytes of the ELF header
+ */
+void print_data(unsigned char *e_ident)
+{
+	printf("Data:                              ");
+	switch (e_ident[EI_DATA])
 	{
 		case ELFDATANONE:
 			printf("NONE\n");
@@ -55,9 +86,35 @@ void print_elf_header_info(Elf64_Ehdr *header)
 
 	printf("Version:                           %d (current)\n",
 			header->e_ident[EI_VERSION]);
-
 	printf("OS/ABI:                            ");
 	switch (header->e_ident[EI_OSABI])
+}
+
+/**
+ * print_version - Prints the version of thr ELF header
+ * @e_ident: The identification bytes of the ELF header
+ */
+void print_version(unsigned char *e_ident)
+{
+	printf("Version:			   %d", e_ident[EI_VERSION]);
+	switch (e_ident[EI_VERSION])
+	{
+		case EV_CURRENT:
+			printf(" (current)\n");
+			break;
+		default:
+			printf("\n");
+	}
+}
+
+/**
+ * print_osabi - Prints the OS/ABI of the ELF header
+ * @e_ident: The identification bytes of the ELF header
+ */
+void print_osabi(unsigned char *e_ident)
+{
+	printf("OS/ABI:                            ");
+	switch (e_ident[EI_OSABI])
 	{
 		case ELFOSABI_SYSV:
 			printf("UNIX - System V\n");
@@ -92,12 +149,32 @@ void print_elf_header_info(Elf64_Ehdr *header)
 		default:
 			printf("<unknown: %x>\n", header->e_ident[EI_OSABI]);
 	}
-
 	printf("ABI Version:                       %d\n",
-			header->e_ident[EI_ABIVERSION]);
-
+		header->e_ident[EI_ABIVERSION]);
 	printf("Type:                              ");
 	switch (header->e_type)
+}
+
+/**
+ * print_abiversion - Prints the ABI version of the ELF header
+ * @e_ident: The identification bytes of the ELF header
+ */
+void print_abiversion(unsigned char *e_ident)
+{
+	printf("ABI Version:			   %d\n", e_ident[EI_ABIVERSION]);
+}
+
+/**
+ * print_type - Prints the type of the ELF header
+ * @e_type: The type of the ELF file
+ * @e_ident: The identification bytes of the ELF header
+ */
+void print_type(uint16_t e_type, unsigned char *e_ident)
+{
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
+		e_type = e_type >> 8;
+	printf("Type:                              ");
+	switch (e_type)
 	{
 		case ET_NONE:
 			printf("NONE (None)\n");
@@ -117,11 +194,31 @@ void print_elf_header_info(Elf64_Ehdr *header)
 		default:
 			printf("<unknown: %x>\n", header->e_type);
 	}
-
 	printf("Entry point address:               0x%lx\n",
 			(unsigned long) header->e_entry);
 }
 
+/**
+ * print_entry - Prints the entry point address of the ELF HEADER
+ * @e_entry: The entry point address
+ * @e_ident: The identification bytes of the ELF header
+ */
+void print_entry(uint64_t e_entry, unsigned char *e_ident)
+{
+	printf(" Entry point address:              ");
+	if (e_ident[EI_CLASS] == ELFCLASS32)
+		printf("%#x\n", (unsigned int)e_entry);
+	else
+		printf("%#lx\n", e_entry);
+}
+
+/**
+ * main - entry point
+ * @argc: arguments
+ * @argv: argument count
+ *
+ * Return: 0 Always
+ */
 int main(int argc, char *argv[])
 {
 	int fd;
